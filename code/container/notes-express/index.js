f3949baaada1361ef5a516206f578a8e83d1575c
Robin Luley <robin.luley@incloud.de>
2021-01-12 17:59:08 +0100

    Add initial code for notes-express

---
+++ code/container/notes-express/index.js
@@ -0,0 +1,94 @@
+const express = require("express");
+var bodyParser = require("body-parser");
+
+const randomTimeout = (onResolve) => {
+  const randomTime = 2000;
+
+  return new Promise((resolve, reject) =>
+    setTimeout(() => {
+      resolve(randomTime);
+    }, randomTime)
+  );
+};
+
+const notes = [
+  {
+    id: 1,
+    title: "Test note 1",
+    content: "I am the test content.",
+  },
+  {
+    id: 2,
+    title: "Test note 2",
+    content: "I am the test content.",
+  },
+  {
+    id: 3,
+    title: "Test note 3",
+    content: "I am the test content.",
+  },
+  {
+    id: 4,
+    title: "Test note 4",
+    content: "I am the test content.",
+  },
+  {
+    id: 5,
+    title: "Test note 5",
+    content: "I am the test content.",
+  },
+];
+
+const app = express();
+app.use(bodyParser.json());
+
+const port = 3000;
+
+// get all notes
+app.get("/notes", async (req, res) => {
+  await randomTimeout();
+  res.send(notes);
+});
+
+// get one note
+app.get("/notes/:id", async (req, res) => {
+  const { id } = req.params;
+
+  await randomTimeout();
+  const note = notes.find((note) => note.id == id);
+  console.log(note);
+
+  if (!note) {
+    return res.send("Note could not be found");
+  }
+
+  res.send(note);
+});
+
+// update note
+app.put("/notes/:id", async (req, res) => {
+  const { id } = req.params;
+  const noteToUpdate = req.body;
+
+  // create item in database
+  await randomTimeout();
+  const updatedNote = { id, ...noteToUpdate };
+
+  res.send(updatedNote);
+});
+
+// create note
+app.post("/notes/", async (req, res) => {
+  const { id } = req.params;
+  const noteToCreate = req.body;
+
+  // create item in database
+  await randomTimeout();
+  const createdNote = { ...noteToCreate, id: notes.length };
+
+  res.send(createdNote);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
---
+++ code/container/notes-express/push.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker push 270501904710.dkr.ecr.eu-central-1.amazonaws.com/notes-express:latest
