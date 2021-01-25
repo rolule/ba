0bfba9aed2a54099180443a435009b23e97d3ee0
Robin Luley <robin.luley@incloud.de>
2021-01-25 18:39:43 +0100

    Add correct installation of docker

--- code/tests/setup.sh
+++ code/tests/setup.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-
 # run this on your ECS instance
-sudo yum install -y k6 git docker
+
+sudo yum update -y
+sudo yum install -y k6 git
+
+# install docker
+sudo amazon-linux-extras install docker
+sudo yum install docker
+sudo service docker start
+sudo usermod -a -G docker ec2-user
+
 
 # install node
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
