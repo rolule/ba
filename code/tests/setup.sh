ab70f638adc245a0274cb6b08e6d0e5e1b288ee0
Robin Luley <robin.luley@incloud.de>
2021-01-25 18:50:50 +0100

    Add installation of python to setup

--- code/tests/setup.sh
+++ code/tests/setup.sh
@@ -15,3 +15,10 @@ sudo usermod -a -G docker ec2-user
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install node
+
+# install python
+sudo yum install -y python3
+python3 -m venv pandas/venv
+source pandas/venv/bin/activate
+pip install pip --upgrade
+pip install pandas matplotlib openpyxl
