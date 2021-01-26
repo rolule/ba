db5da525663cd24b45851859fa5536143ae647a6
Robin Luley <robin.luley@incloud.de>
2021-01-26 13:03:01 +0100

    Comparing multiple response times

--- code/tests/pandas/main.py
+++ code/tests/pandas/main.py
@@ -1,10 +1,5 @@
-import pandas as pd
 import matplotlib.pyplot as plt
-
-# pip install pandas matplotlib openpyxl
-
-data = pd.read_csv('test.csv')
-#data.info()
+import pandas as pd
 
 # columns
 # metric_name | timestamp | metric_value | check | error | error_code | group | method | name | proto | scenario |
@@ -15,21 +10,43 @@ data = pd.read_csv('test.csv')
 # data_sent, data_received, iteration_duration, iterations, vus, max_vus
 
 
-reqs = data.loc[data['metric_name'] == 'http_req_duration', ['timestamp', 'metric_value']]
-# print(reqs['metric_value'].describe())
+# csv result files
+files = ['../debug/fargate/a/128/light-load/0126-1236.csv', '../debug/fargate/a/128/test/0125-1704.csv',
+         '../debug/fargate/a/128/test/0125-1711.csv']
 
-min = reqs['metric_value'].min()
-median = reqs['metric_value'].median()
-max = reqs['metric_value'].max()
+# names of the plot graphs
+names = ["Fargate 1", "Fargate 2", "Fargate 3"]
 
-print(f'Min: {min}')
-print(f'Median: {median}')
-print(f'Max: {max}')
+# read files
+tables = [pd.read_csv(file) for file in files]
 
-reqs.plot(x='timestamp', y="metric_value", title="Response time", kind="box")
-plt.xlabel("Time (s)")
+# create time column
+for table in tables:
+    table["time"] = table["timestamp"] - table["timestamp"][0]
+
+# filter for request duration metric
+tables = [table[table["metric_name"] == "http_req_duration"] for table in tables]
+
+# only show time and metric value
+tables = [table[["time", "metric_value"]] for table in tables]
+
+# rename metric_value column to supplied name
+tables = [table.rename(columns={"metric_value": name}) for table, name in zip(tables, names)]
+
+# merge all tables together
+merge = tables[0]
+for i in range(1, len(tables)):
+    merge = pd.merge(merge, tables[i], how="left", on="time")
+
+# set time column to be the index
+merge = merge.set_index("time")
+print(merge.describe())
+
+# plot merged table
+merge.plot()
+plt.xlabel("Zeit (s)")
 plt.ylabel("Response Time (ms)")
 plt.show()
-#plt.savefig('plot')
 
-#reqs.to_excel('test.xlsx')
+# saves the plot under the given path
+# plt.savefig('plot')
